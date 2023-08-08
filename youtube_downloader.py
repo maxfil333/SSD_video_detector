13f2a5ef3e55c9189c847779df01d5652cc2fd65
maxfil333 <maxfil333@gmail.com>
2023-08-08 21:29:08 +0300

    youtube_downloader.py added

---
+++ youtube_downloader.py
@@ -0,0 +1,21 @@
+import pytube
+import os
+
+
+def download_video(video_url, save_path='downloaded_videos'):
+    """Скачивает видео с youtube по заданному url"""
+
+    os.makedirs('downloaded_videos', exist_ok=True)
+    try:
+        yt = pytube.YouTube(video_url)
+        print(f'{yt.title=}')
+        print(f'{yt.length=}')
+        yt.streams.get_lowest_resolution().download(output_path=save_path)
+        print("Видео успешно скачано.")
+    except Exception as e:
+        print("Произошла ошибка при скачивании видео:", e)
+
+
+if __name__ == '__main__':
+    url = str(input('Введите url ... ')).strip()
+    download_video(video_url=url)
