9e6fc0ae4cf5c45d9a10f35d9ac7d8ad9058e646
maxfil333 <maxfil333@gmail.com>
2023-08-27 19:49:58 +0300

    add get_by_resolution

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -10,7 +10,8 @@ def download_video(video_url, save_path='downloaded_videos'):
         yt = pytube.YouTube(video_url)
         print(f'{yt.title=}')
         print(f'{yt.length=}')
-        yt.streams.get_lowest_resolution().download(output_path=save_path)
+        # yt.streams.get_lowest_resolution().download(output_path=save_path)
+        yt.streams.get_by_resolution(resolution='720p').download(output_path=save_path)
         print("Видео успешно скачано.")
     except Exception as e:
         print("Произошла ошибка при скачивании видео:", e)
